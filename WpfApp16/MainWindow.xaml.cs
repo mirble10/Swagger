6dcb7f1bcc2860fa0e384c9d86251888e0579f74
stud <stud@PK5111-2>
2025-04-18 10:52:15 +0400

    Добавьте файлы проекта.

---
+++ WpfApp16/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using ConsoleApp32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp16
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Auth auth = new Auth();
+            auth.Auth1();
+
+            cmbList.ItemsSource = auth.otvet;
+            
+
+               
+        }
+    }
+}
---
+++ WpfApp16/Metod/Auth.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ConsoleApp32
+{
+    public class Auth
+    {
+        public string[] otvet { get; set; }
+        private string Token {  get; set; }
+
+        public void Auth1() 
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Post, "http://192.168.59.39:12345/api/Auth/GetToken");
+            var content = new StringContent("{\r\n  \"name\": \"t\",\r\n  \"password\": \"123\",\r\n  \"login\": \"admin\"\r\n}", null, "application/json");
+            request.Content = content;
+            var response = client.SendAsync(request).Result;
+            response.EnsureSuccessStatusCode();
+            string token = response.Content.ReadAsStringAsync().Result;
+            Token = token;
+            Examination($"http://192.168.59.39:12345/api/Groups?token={Token}");
+        }
+        private void Examination(string token)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, token);
+            var response = client.SendAsync(request).Result;
+            
+
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                
+                Auth2($"http://192.168.59.39:12345/api/Groups?token={Token}");
+
+            }
+            else
+            {
+                Auth1();
+            }
+         
+        }
+        private void Auth2(string token)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, token);
+            var response =  client.SendAsync(request).Result;
+            response.EnsureSuccessStatusCode();
+            var a = JsonConvert.DeserializeObject<string[]>(response.Content.ReadAsStringAsync().Result);
+            otvet = a;
+        
+        }
+
+    }
+}
